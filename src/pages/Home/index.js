34114e9691e0125adcae590070876e99f30ee41d
isagrt <isadoragoedert05@gmail.com>
2023-09-19 09:11:21 -0300

    alteração do icon

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
 
+import Header from "../../components/Home/Header";
+
 export default function Home(){
     return (
     <View style={styles.container}>
         <Text>
-            Página Home
+            <Header/>
         </Text>
     </View>
     );
