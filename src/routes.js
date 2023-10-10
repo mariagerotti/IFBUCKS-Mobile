bb35a5f4953e22851a699ceb986b15845fe00787
isagrt <isadoragoedert05@gmail.com>
2023-10-10 08:49:36 -0300

    alt

--- src/routes.js
+++ src/routes.js
@@ -8,7 +8,7 @@ import Profile from './pages/Profile';
 import Login from './pages/Login';
 
 import CartButton from "./components/Navigation/CartButton";
-import AsyncStorage from "@react-native-async-storage/async-storage"; // Importe AsyncStorage
+import AsyncStorage from "@react-native-async-storage/async-storage"; 
 
 const Tab = createBottomTabNavigator();
 
@@ -16,15 +16,15 @@ export default function Routes() {
   const [loggedIn, setLoggedIn] = useState(false);
 
   useEffect(() => {
-    // Verifique se há um token de autenticação armazenado em AsyncStorage (isso é apenas um exemplo)
+
     const checkLoginStatus = async () => {
       try {
         const token = await AsyncStorage.getItem('authToken');
         if (token) {
-          // Se houver um token, o usuário está logado
+        
           setLoggedIn(true);
         } else {
-          // Se não houver um token, o usuário não está logado
+
           setLoggedIn(false);
         }
       } catch (error) {
@@ -32,9 +32,9 @@ export default function Routes() {
       }
     };
 
-    // Chame a função de verificação de login
+  
     checkLoginStatus();
-  }, []); // Certifique-se de fornecer um array de dependências vazio para que o useEffect seja executado apenas uma vez
+  }, []); 
 
   return (
     <Tab.Navigator
