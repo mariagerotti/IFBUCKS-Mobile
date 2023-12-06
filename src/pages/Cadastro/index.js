45d1eeeaeb264078e84a7914c30184b8fc0b7c3a
isagrt <isadoragoedert05@gmail.com>
2023-12-06 14:12:28 -0300

    adicionando imagem ao cadastro

--- src/pages/Cadastro/index.js
+++ src/pages/Cadastro/index.js
@@ -1,27 +1,59 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity, Image, StyleSheet } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+import ImagePicker from 'react-native-image-picker';
 
 export default function Cadastro() {
   const [nome, setNome] = useState('');
   const [idade, setIdade] = useState('');
   const [email, setEmail] = useState('');
   const [cpf, setCpf] = useState('');
+  const [perfilImage, setPerfilImage] = useState(null);
 
   const navigation = useNavigation();
 
   const handleCadastro = () => {
-   
+    
     console.log('Nome:', nome);
     console.log('Idade:', idade);
     console.log('Email:', email);
     console.log('CPF:', cpf);
+    console.log('Imagem de Perfil:', perfilImage);
+  };
+
+  const handleImagePicker = () => {
+    ImagePicker.showImagePicker(
+      {
+        title: 'Escolha uma imagem',
+        storageOptions: {
+          skipBackup: true,
+          path: 'images',
+        },
+      },
+      (response) => {
+        if (response.didCancel) {
+          console.log('Usuário cancelou a escolha de imagem');
+        } else if (response.error) {
+          console.error('Erro ao escolher a imagem:', response.error);
+        } else {
+          
+          setPerfilImage(response.uri);
+        }
+      }
+    );
   };
 
   return (
     <View style={styles.container}>
       <View style={styles.formContainer}>
         <Text style={styles.title}>Cadastro</Text>
+        <TouchableOpacity onPress={handleImagePicker}>
+          {perfilImage ? (
+            <Image source={{ uri: perfilImage }} style={styles.perfilImage} />
+          ) : (
+            <Text style={styles.perfilImagePlaceholder}>Adicionar Foto de Perfil</Text>
+          )}
+        </TouchableOpacity>
         <TextInput
           style={styles.input}
           placeholder="Nome"
@@ -64,13 +96,14 @@ const styles = StyleSheet.create({
     padding: 30,
     borderRadius: 10,
     width: '80%',
-    height: '60%', 
+    height: '70%', 
     justifyContent: 'flex-end',
+    alignItems: 'center',
   },
   title: {
     fontSize: 24,
     fontWeight: 'bold',
-    marginBottom: 30,
+    marginBottom: 20,
   },
   input: {
     height: 40,
@@ -79,6 +112,7 @@ const styles = StyleSheet.create({
     borderRadius: 5,
     paddingHorizontal: 10,
     marginBottom: 10,
+    width: '100%',
   },
   cadastroButton: {
     backgroundColor: '#8fb190',
@@ -86,10 +120,22 @@ const styles = StyleSheet.create({
     marginTop: 10,
     borderRadius: 5,
     alignItems: 'center',
+    width: '100%',
   },
   cadastroButtonText: {
     color: 'white',
     fontSize: 18,
     fontWeight: 'bold',
+  },
+  perfilImage: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+    marginBottom: 10,
+  },
+  perfilImagePlaceholder: {
+    fontSize: 16,
+    color: '#8fb190',
+    marginBottom: 10,
   },
 });
